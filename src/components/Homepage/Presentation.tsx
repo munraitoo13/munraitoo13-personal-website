52f841f52401c77fdfb63e711df778ec83de60cb
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-06-18 16:36:09 -0300

    folder name change

---
+++ src/components/Homepage/Presentation.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Presentation() {
+  return (
+    <div className="flex w-1/2 flex-col items-center justify-center text-center">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1, transition: { duration: 0.5 } }}
+      >
+        <h1 className="mb-5 text-6xl font-bold">
+          What's up!
+          <br />
+          My name is <span className="text-red-600">Augusto</span>.
+        </h1>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+          transition: { duration: 0.5, delay: 0.2 },
+        }}
+      >
+        <p className="text-lg">
+          <span className="opacity-75">
+            Also known as munraitoo13, I'm a passionate
+          </span>
+          <b> Software Engineer </b>
+          <span className="opacity-75">from Brazil!</span>
+        </p>
+      </motion.div>
+    </div>
+  );
+}
---
+++ src/components/Homepage/Qualities.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import {
+  IconBrandHipchat,
+  IconBriefcase2,
+  IconStars,
+} from "@tabler/icons-react";
+import { motion } from "framer-motion";
+
+export default function Qualities() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{
+        opacity: 1,
+        scale: 1,
+        transition: { duration: 0.5 },
+      }}
+      className="flex w-full justify-center px-20 pb-20"
+    >
+      <div className="flex flex-col items-center gap-5 lg:flex-row">
+        {/* comunicative */}
+        <div className="max-w-sm rounded-xl border border-neutral-900 p-3 text-white">
+          <div className="flex items-center">
+            <IconBrandHipchat
+              size={24}
+              stroke={1.25}
+              className="z-10 box-content min-w-fit rounded-full bg-red-600 p-3"
+            />
+
+            <h3 className="relative right-5 w-full rounded-r-full bg-neutral-900 py-2 pl-10 font-bold">
+              Communicative
+            </h3>
+          </div>
+
+          <p className="p-3 opacity-75">
+            I can communicate clearly. A must-have quality that enhances the
+            entire team working experience.
+          </p>
+        </div>
+
+        {/* competent */}
+        <div className="max-w-sm rounded-xl border border-neutral-900 p-3 text-white">
+          <div className="flex items-center">
+            <IconBriefcase2
+              size={24}
+              stroke={1.25}
+              className="z-10 box-content min-w-fit rounded-full bg-red-600 p-3"
+            />
+
+            <h3 className="relative right-5 w-full rounded-r-full bg-neutral-900 py-2 pl-10 font-bold">
+              Competent
+            </h3>
+          </div>
+
+          <p className="p-3 opacity-75">
+            I'm dedicated. Another must-have trait that significantly enhances
+            the overall working experience.
+          </p>
+        </div>
+
+        {/* detail-oriented */}
+        <div className="max-w-sm rounded-xl border border-neutral-900 p-3 text-white">
+          <div className="flex items-center">
+            <IconStars
+              size={24}
+              stroke={1.25}
+              className="z-10 box-content min-w-fit rounded-full bg-red-600 p-3"
+            />
+
+            <h3 className="relative right-5 w-full rounded-r-full bg-neutral-900 py-2 pl-10 font-bold">
+              Detail-oriented
+            </h3>
+          </div>
+
+          <p className="p-3 opacity-75">
+            I have good coding and security practises. A very important quality
+            that ensures the success of the final product.
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
