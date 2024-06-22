08c8e8da56b736af8b99ef6bf941f95192b1a7b8
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-06-22 04:07:51 -0300

    fixed configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 import nextMDX from "@next/mdx";
-import rehypeSlug from "rehype-slug";
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [rehypeSlug],
+    rehypePlugins: [],
   },
 });
 const nextConfig = {
