6074377b15881474e9519b9c223e150ed6b83dc7
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-06-26 03:56:41 -0300

    experience page development starts

--- src/app/personal/blog/page.tsx
+++ src/app/personal/blog/page.tsx
@@ -1,19 +1,29 @@
 import Header from "@/components/General/Header";
+import Markdown from "@/components/Pages/Markdown";
+import Contents from "@/components/Pages/TableOfContents";
+import Content from "./content.mdx";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Blog | munraitoo13",
-  description: "munraitoo13's blog.",
+  description: "munraitoo13's blog",
 };
 
-export default function Blog() {
+export default async function Blog() {
   return (
     <>
       <Header
         cathegory="Personal"
         pageTitle="Blog"
-        pageDescription="Personal blog posts about technology and more."
+        pageDescription="All of my blog posts in one place."
       />
+
+      <div className="flex flex-col gap-3 lg:flex-row-reverse lg:justify-between">
+        <Contents />
+        <Markdown>
+          <Content />
+        </Markdown>
+      </div>
     </>
   );
 }
--- src/app/professional/experiences/page.tsx
+++ src/app/professional/experiences/page.tsx
@@ -1,19 +1,29 @@
-import { Metadata } from "next";
 import Header from "@/components/General/Header";
+import Markdown from "@/components/Pages/Markdown";
+import Contents from "@/components/Pages/TableOfContents";
+import Content from "./content.mdx";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Experiences | munraitoo13",
-  description: "munraitoo13's experiences.",
+  description: "munraitoo13's experiences",
 };
 
-export default function Experience() {
+export default async function Experiences() {
   return (
     <>
       <Header
         cathegory="Professional"
         pageTitle="Experiences"
-        pageDescription="All the professional experiences I had in the past."
+        pageDescription="My experiences in the professional world."
       />
+
+      <div className="flex flex-col gap-3 lg:flex-row-reverse lg:justify-between">
+        <Contents />
+        <Markdown>
+          <Content />
+        </Markdown>
+      </div>
     </>
   );
 }
