4e212d678b1703f8e89864fa3915f9edf1eb2e41
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-08-29 21:22:49 -0300

    fixed messed up scroll

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -4,20 +4,26 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+interface Headings {
+  id: string;
+  text: string;
+  headingLevel: string;
+}
+
 export default function TableOfContents() {
-  const [headings, setHeadings] = useState<
-    { id: string; text: string; headingLevel: string }[]
-  >([]);
+  const [headings, setHeadings] = useState<Headings[]>([]);
 
   useEffect(() => {
+    // find all headings in the content
     const foundHeadings = document.querySelectorAll(
       ".mdx-content h2, .mdx-content h3",
     );
 
+    // creates an array of objects with the heading details and map it to the state
     const headingDetails = Array.from(foundHeadings).map((heading) => {
       return {
         id: heading.id,
-        text: heading.textContent || "", // Ensure text is not null
+        text: heading.textContent!,
         headingLevel: heading.tagName,
       };
     });
@@ -27,7 +33,7 @@ export default function TableOfContents() {
 
   return (
     <motion.nav
-      className="mb-5 flex h-fit w-full flex-col gap-5 text-nowrap rounded-xl border border-red-600 p-10 lg:w-fit"
+      className="mb-5 flex h-fit w-full flex-col gap-5 text-nowrap rounded-xl border border-red-600 p-10 lg:sticky lg:top-0 lg:w-fit"
       initial={{ x: 50, opacity: 0 }}
       animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}
     >
