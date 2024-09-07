05fcb8d5bd7dd4a783048101f3d2d7b1551851dd
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-09-07 10:54:49 -0300

    fixed trycatch prisma

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,14 +2,18 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-    include: {
-      tags: true,
-    },
-  });
-
-  return NextResponse.json(posts);
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      include: {
+        tags: true,
+      },
+    });
+    return NextResponse.json(posts);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
 }
