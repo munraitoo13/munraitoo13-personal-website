51786edc335eaf3a89d2974c31fbd8d33fa17524
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-09-07 18:08:48 -0300

    revert favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,27 +16,6 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-      </head>
       <body
         className={`${inter.className} absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] text-sm text-neutral-700 [background-size:16px_16px] dark:bg-neutral-950 dark:bg-[radial-gradient(#101010_1px,transparent_1px)] dark:text-neutral-300`}
       >
