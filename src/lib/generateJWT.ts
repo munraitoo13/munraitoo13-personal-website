1e7ba51e7360cbe17ce402449105431b5194be60
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-10-19 16:34:37 -0300

    more bugs

--- src/lib/generateJWT.ts
+++ src/lib/generateJWT.ts
@@ -1,14 +1,13 @@
-"use server";
-
 import { JWTPayload, SignJWT } from "jose";
 import { getJwtKey } from "@/utils/getJwtKey";
 
 export async function generateJwt(payload: JWTPayload) {
   const key = getJwtKey();
-
-  return await new SignJWT(payload)
+  const token = await new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
     .setExpirationTime("2h")
     .sign(key);
+
+  return token;
 }
