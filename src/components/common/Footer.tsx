bde608254c9fbd6b81eb5c4786394cc2f5619ceb
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-11-27 02:01:22 -0300

    footer redesign

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -10,63 +10,65 @@ export function Footer() {
   const professionalLinks = getProfessionalLinks(t);
 
   return (
-    <footer className="flex w-full flex-col items-center bg-gradient-to-b from-white px-10 pb-24 pt-48 dark:from-neutral-950">
-      <div className="flex w-full max-w-screen-lg flex-col gap-10">
-        {/* top */}
-        <div className="flex flex-col justify-between gap-10 lg:flex-row">
-          {/* navigation */}
-          <div className="flex flex-col gap-10 lg:flex-row">
-            {/* personal */}
-            <div className="flex flex-col items-center gap-3">
-              <p className="w-fit text-nowrap rounded-full bg-neutral-50 px-5 py-2 dark:bg-neutral-900/25">
-                {t("personal")}
-              </p>
+    <footer className="flex w-full flex-col items-center bg-gradient-to-b from-white px-10 pb-20 pt-48 dark:from-neutral-950">
+      <div className="flex w-full max-w-screen-lg flex-col justify-between gap-10 lg:flex-row">
+        {/* navigation */}
+        <div className="flex flex-col gap-10 lg:flex-row">
+          {/* personal */}
+          <div className="flex flex-col items-center gap-3">
+            <p className="w-fit text-nowrap rounded-full bg-neutral-50 px-5 py-2 dark:bg-neutral-900/25">
+              {t("personal")}
+            </p>
 
-              {personalLinks.map(({ name, href }) => (
-                <Link
-                  key={name}
-                  href={href}
-                  className="text-neutral-900 hover:text-red-500 dark:text-white"
-                >
-                  {name}
-                </Link>
-              ))}
-            </div>
-
-            {/* professional */}
-            <div className="flex flex-col items-center gap-3">
-              <p className="w-fit text-nowrap rounded-full bg-neutral-50 px-5 py-2 dark:bg-neutral-900/25">
-                {t("professional")}
-              </p>
-
-              {professionalLinks.map(({ name, href }) => (
-                <Link
-                  key={name}
-                  href={href}
-                  className="text-neutral-900 hover:text-red-500 dark:text-white"
-                >
-                  {name}
-                </Link>
-              ))}
-            </div>
+            {personalLinks.map(({ name, href }) => (
+              <Link
+                key={name}
+                href={href}
+                className="text-neutral-900 hover:text-red-500 dark:text-white"
+              >
+                {name}
+              </Link>
+            ))}
           </div>
 
-          {/* about-me */}
-          <div className="mx-auto flex max-w-lg flex-col items-center gap-2 lg:mx-0">
-            <p className="w-fit rounded-full bg-neutral-50 px-5 py-2 dark:bg-neutral-900/25">
-              {t("about")}
+          {/* professional */}
+          <div className="flex flex-col items-center gap-3">
+            <p className="w-fit text-nowrap rounded-full bg-neutral-50 px-5 py-2 dark:bg-neutral-900/25">
+              {t("professional")}
             </p>
 
-            <p className="text-center text-neutral-900 dark:text-white">
-              {t("aboutPhrase")}
-            </p>
-
-            <Socials />
+            {professionalLinks.map(({ name, href }) => (
+              <Link
+                key={name}
+                href={href}
+                className="text-neutral-900 hover:text-red-500 dark:text-white"
+              >
+                {name}
+              </Link>
+            ))}
           </div>
         </div>
 
-        {/* copy */}
-        <p className="mx-auto">&copy; 2024, munraitoo13</p>
+        {/* about-me */}
+        <div className="mx-auto flex max-w-lg flex-col items-center gap-2 lg:mx-0">
+          {/* title */}
+          <p className="w-fit rounded-full bg-neutral-50 px-5 py-2 dark:bg-neutral-900/25">
+            {t("about")}
+          </p>
+
+          {/* phrase */}
+          <p className="text-center text-neutral-900 dark:text-white">
+            {t("aboutPhrase")}
+          </p>
+
+          {/* socials */}
+          <Socials />
+
+          {/* copy */}
+          <p className="mx-auto text-neutral-400 dark:text-neutral-700">
+            &copy; 2024, munraitoo13
+          </p>
+        </div>
       </div>
     </footer>
   );
