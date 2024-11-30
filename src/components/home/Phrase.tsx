8dff4dec89b6893ae204072ee8d35ce7d0115cd8
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-11-30 00:30:33 -0300

    home phrase update

--- src/components/home/Phrase.tsx
+++ src/components/home/Phrase.tsx
@@ -1,18 +1,26 @@
 import { MotionBlockquote } from "@/components/common/Motion";
 import { useTranslations } from "next-intl";
 import { phraseVariants } from "@/animations/motionVariants";
+import { IconQuoteFilled } from "@tabler/icons-react";
 
 export function Phrase() {
   const t = useTranslations("HomePage");
 
   return (
     <MotionBlockquote
-      className="mx-auto mt-36 flex w-fit flex-col gap-1 text-3xl font-light md:text-5xl"
+      className="mx-auto mt-36 flex w-fit text-3xl font-light md:text-5xl"
       variants={phraseVariants}
       initial="hidden"
       animate="visible"
     >
-      <div className="flex w-fit flex-col gap-1">
+      <IconQuoteFilled
+        className="-mr-24 hidden -scale-100 text-neutral-200/50 md:block dark:text-neutral-900/25"
+        size={256}
+        stroke={0.5}
+      />
+
+      {/* flex */}
+      <div className="z-10 flex w-fit flex-col gap-1">
         {/* simple */}
         <span className="italic">{t("simple")} </span>
 
