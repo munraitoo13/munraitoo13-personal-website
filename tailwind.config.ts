cae34cb9b4a195eb08dd5fb831fbfcf0048cba90
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-12-08 00:31:07 -0300

    new tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,22 +6,19 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "selector",
-  plugins: [require("@tailwindcss/typography")],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "radial-custom":
-          "radial-gradient(circle, #e5e7eb 1px, transparent 1px)",
 
-        "radial-custom-dark":
-          "radial-gradient(circle, #101010 1px, transparent 1px)",
-      },
-      backgroundSize: {
-        "16": "16px 16px",
-      },
+  darkMode: "selector",
+
+  theme: {
+    colors: {
+      text: "rgb(var(--text))",
+      background: "rgb(var(--background))",
+      primary: "rgb(var(--primary))",
+      secondary: "rgb(var(--secondary))",
     },
   },
+
+  plugins: [require("@tailwindcss/typography")],
 };
 
 export default config;
