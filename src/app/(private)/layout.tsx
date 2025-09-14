ee7780fbf6dbbf01a8d2fafce3ea97d14535e8b3
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2025-09-14 20:12:46 -0300

    fixed vh for private routes

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex min-h-dvh flex-col items-center justify-center">
         {children}
       </main>
 
